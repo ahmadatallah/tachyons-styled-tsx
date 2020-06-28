065e07974292a886bb6afb6642ffafb8dc833449
ahmadatallah <ahmad.atallah@yandex.com>
2020-06-28 10:36:04 +0200

    Update app meta

--- src/jsx.ts
+++ src/jsx.ts
@@ -5,10 +5,11 @@ import { jsx as emotionJSX } from "@emotion/core";
 import "types/react-jsx";
 
 type Element = keyof typeof elements;
-const tags: unknown = {};
 type Tags = {
   [key: string]: React.ComponentType<any> | string | undefined;
 };
+
+const tags: Tags = {};
 
 const isValidKey = (key: unknown): key is Element => {
   return typeof key === "string";
@@ -16,12 +17,12 @@ const isValidKey = (key: unknown): key is Element => {
 
 Object.keys(elements).forEach((key) => {
   if (isValidKey(key)) {
-    (tags as Tags)[(key as string).toLowerCase()] = elements[key] as React.ComponentType<any>;
+    tags[key.toLowerCase()] = elements[key];
   }
 });
 
 export const jsx = (type: any, props: any, ...children: any[]) => {
-  return emotionJSX.apply(undefined, [(tags as Tags)[type] ?? type, props, ...children]);
+  return emotionJSX.apply(undefined, [tags[type] ?? type, props, ...children]);
 };
 
 export default jsx;
